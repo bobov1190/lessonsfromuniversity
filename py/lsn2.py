60df914e3d83defa88124028517b2c8bd4a6e259
Bryan <109802068+bobov1190@users.noreply.github.com>
2025-01-23 12:20:21 +0500

    Create lsn2.py

---
+++ py/lsn2.py
@@ -0,0 +1,34 @@
+py = "Python"
+
+# a = 12
+# b = 10
+
+# print(py)
+
+# print(f"when we divide {a} to {b}")
+
+student_name = "Bahodir"
+age = 18
+math_points = 27
+
+# print(f"Name:{student_name:<10} Age:{age:<10} Points:{math_points:<10}")
+
+
+# print("Please input a message: ")
+# a = int(input())
+# print(a**2)
+
+# #Casting str() int() and etc
+
+
+# # to Farengeit
+# print("Please inpput your temperature in F:")
+# a = int(input())
+# print("The temperature in C is: ")
+# print((a-30)/2)
+
+print(f"Languages i know:\n-Python\n-Java\n-Cd\n\n\n")
+
+print(f"Sum: 7+3={7+3}\nDifference: 7-3={7-3}\nProduct: 7*3={7*3}\nQuotient: 7/3={7/3}\n\n\n")
+
+print(f"Hello, my name is {student_name} and i love {py}")
