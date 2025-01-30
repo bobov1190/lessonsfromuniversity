22a8115ced431d036524fb74a1d4c902d5dfa953
Bryan <109802068+bobov1190@users.noreply.github.com>
2025-01-30 12:20:48 +0500

    111

---
+++ py/lsn3.py
@@ -0,0 +1,36 @@
+# a = int(input("Insert a number: "))
+# b = int(input("Insert a number: "))
+
+# if a > b:
+#     print("a > b")
+# elif a == b:
+#     print('a = b')
+# else:
+#     print("b > a")
+
+
+# c = int(input("num : "))
+
+# if c < 0:
+#     print("num is less than 0")
+
+
+# d = int(input("num : "))
+
+# if d%3 == 0:
+#     print("divisible by 3")
+
+e = int(input("1: num : "))
+f = int(input("2: num : "))
+g = int(input("3: num : "))
+
+if e<f:
+    if e<g:
+        print("1: is the smallest")
+    else:
+        print("3: is the smallest")
+else:
+    if f<g:
+        print("2: is the smallest")
+    else:
+        print("3: is the smallest")
